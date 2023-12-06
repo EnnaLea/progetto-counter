3edfd5868e8ba510674ecb21e3b434f8ab4820a8
EnnaLea <soleariaeve@gmail.com>
2023-12-06 13:06:09 +0100

    formattazione

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,24 +1,22 @@
-const plusButton = document.querySelector('.plus');
-const minusButton = document.querySelector('.minus');
-const resetButton = document.querySelector('.reset');
-const counter = document.querySelector('.counter');
+const plusButton = document.querySelector(".plus");
+const minusButton = document.querySelector(".minus");
+const resetButton = document.querySelector(".reset");
+const counter = document.querySelector(".counter");
 
 //funzione per aumentare il counter
-plusButton.addEventListener('click', () => {
-    let currentValue = parseInt(counter.textContent);
-    counter.textContent = currentValue + 1;
-})
+plusButton.addEventListener("click", () => {
+  let currentValue = parseInt(counter.textContent);
+  counter.textContent = currentValue + 1;
+});
 
 //funzione per decrementare il counter
-minusButton.addEventListener('click', () => {
-    let currentValue = parseInt(counter.textContent);
-        counter.textContent = currentValue - 1;
-    
-})
+minusButton.addEventListener("click", () => {
+  let currentValue = parseInt(counter.textContent);
+  counter.textContent = currentValue - 1;
+});
 
 //funzione per resettare il valore a 0
-resetButton.addEventListener('click', () => {
-    let currentValue = parseInt(counter.textContent);
-    counter.textContent = 0;
-})
-
+resetButton.addEventListener("click", () => {
+  let currentValue = parseInt(counter.textContent);
+  counter.textContent = 0;
+});
