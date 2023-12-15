f4cf4ff316672ad111654df6479a2594000299e7
EnnaLea <soleariaeve@gmail.com>
2023-12-15 20:37:36 +0100

    crea pulsanti e counter con javascript

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,3 +1,24 @@
+const divContainer = document.querySelector('.container');
+
+// Creazione del pulsante di incremento
+let incrementButton = document.createElement("button");
+incrementButton.className='btn btn-info btn-lg plus';
+incrementButton.textContent = "+";
+divContainer.append(incrementButton);
+
+//Creazione del div counter
+let counterButton = document.createElement("div");
+counterButton.className= 'counter';
+counterButton.textContent = '0'
+divContainer.append(counterButton);
+
+// Creazione del pulsante di decremento
+let decrementButton = document.createElement("button");
+decrementButton.className='btn btn-info btn-lg minus';
+decrementButton.textContent = "-";
+divContainer.append(decrementButton);
+
+
 const plusButton = document.querySelector(".plus");
 const minusButton = document.querySelector(".minus");
 const resetButton = document.querySelector(".reset");
