41ba8b6036b733ebaaf8405df91470e2fb1f2ea3
EnnaLea <soleariaeve@gmail.com>
2023-12-15 20:48:58 +0100

    change

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,28 +1,26 @@
-const divContainer = document.querySelector('.container');
+const divContainer = document.querySelector(".container");
+const plusButton = document.querySelector(".plus");
+const minusButton = document.querySelector(".minus");
+const resetButton = document.querySelector(".reset");
+const counter = document.querySelector(".counter");
 
 // Creazione del pulsante di incremento
 let incrementButton = document.createElement("button");
-incrementButton.className='btn btn-info btn-lg plus';
+incrementButton.className = "btn btn-info btn-lg plus";
 incrementButton.textContent = "+";
 divContainer.append(incrementButton);
 
 //Creazione del div counter
 let counterButton = document.createElement("div");
-counterButton.className= 'counter';
-counterButton.textContent = '0'
+counterButton.className = "counter";
+counterButton.textContent = "0";
 divContainer.append(counterButton);
 
 // Creazione del pulsante di decremento
 let decrementButton = document.createElement("button");
-decrementButton.className='btn btn-info btn-lg minus';
+decrementButton.className = "btn btn-info btn-lg minus";
 decrementButton.textContent = "-";
 divContainer.append(decrementButton);
-
-
-const plusButton = document.querySelector(".plus");
-const minusButton = document.querySelector(".minus");
-const resetButton = document.querySelector(".reset");
-const counter = document.querySelector(".counter");
 
 //funzione per aumentare il counter
 plusButton.addEventListener("click", () => {
